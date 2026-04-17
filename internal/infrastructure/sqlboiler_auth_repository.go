46c47957a7b4c9947829664fdaafdacc827acf7c
MegaDoge <gggols32@gmail.com>
2026-04-17 23:16:12 +0400

    style: rename repository files

---
+++ internal/infrastructure/sqlboiler_auth_repository.go
@@ -0,0 +1,67 @@
+package infrastructure
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/aarondl/sqlboiler/v4/boil"
+	"github.com/megadoge1337/maxon/internal/domain"
+	"github.com/megadoge1337/maxon/models"
+)
+
+type SqlBoilerAuthRepository struct {
+	db *sql.DB
+}
+
+func NewSqlBoilerAuthRepository(db *sql.DB) *SqlBoilerAuthRepository {
+	return &SqlBoilerAuthRepository{
+		db: db,
+	}
+}
+
+func sessionModelToDomain(m models.Session) domain.Session {
+	return domain.Session{
+		ID:        m.ID,
+		UserID:    m.UserID,
+		ExpiresAt: m.ExpiresAt,
+		CreatedAt: m.CreatedAt,
+	}
+}
+
+func sessionDomainToModel(d domain.Session) models.Session {
+	return models.Session{
+		ID:        d.ID,
+		UserID:    d.UserID,
+		ExpiresAt: d.ExpiresAt,
+		CreatedAt: d.CreatedAt,
+	}
+}
+
+func (r *SqlBoilerAuthRepository) Create(ctx context.Context, seesionDomain domain.Session) (*domain.Session, error) {
+	sessionModel := sessionDomainToModel(seesionDomain)
+	err := sessionModel.Insert(ctx, r.db, boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+	seesionDomain = sessionModelToDomain(sessionModel)
+	return &seesionDomain, nil
+}
+
+func (r *SqlBoilerAuthRepository) GetById(ctx context.Context, id string) (*domain.Session, error) {
+	sessionModel, err := models.FindSession(ctx, r.db, id)
+	if err != nil {
+		return nil, err
+	}
+	sessionDomain := sessionModelToDomain(*sessionModel)
+	return &sessionDomain, nil
+}
+
+func (r *SqlBoilerAuthRepository) DeleteById(ctx context.Context, sessionDomain domain.Session) (*domain.Session, error) {
+	sessionModel := sessionDomainToModel(sessionDomain)
+	_, err := sessionModel.Delete(ctx, r.db)
+	if err != nil {
+		return nil, err
+	}
+	sessionDomain = sessionModelToDomain(sessionModel)
+	return &sessionDomain, nil
+}
---
+++ internal/infrastructure/sqlboiler_user_repository.go
@@ -0,0 +1,114 @@
+package infrastructure
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/aarondl/null/v8"
+	"github.com/aarondl/sqlboiler/v4/boil"
+	"github.com/aarondl/sqlboiler/v4/queries/qm"
+	"github.com/megadoge1337/maxon/internal/domain"
+	"github.com/megadoge1337/maxon/models"
+)
+
+type SqlBoilerUserRepository struct {
+	db *sql.DB
+}
+
+func NewSqlBoilerUserRepository(db *sql.DB) *SqlBoilerUserRepository {
+	return &SqlBoilerUserRepository{
+		db: db,
+	}
+}
+
+func userModelToDomain(m models.User) domain.User {
+	return domain.User{
+		ID:       m.ID,
+		Username: m.Username,
+		Email:    m.Email,
+		Password: m.Password,
+		Created:  m.Created.Time,
+	}
+}
+
+func userModelsToDomains(ms models.UserSlice) []domain.User {
+	var ds []domain.User
+	for _, m := range ms {
+		ds = append(ds, domain.User{
+			ID:       m.ID,
+			Username: m.Username,
+			Email:    m.Email,
+			Password: m.Password,
+			Created:  m.Created.Time,
+		})
+	}
+	return ds
+}
+
+func userDomainToModel(d domain.User) models.User {
+	return models.User{
+		ID:       d.ID,
+		Username: d.Username,
+		Email:    d.Email,
+		Password: d.Password,
+		Created:  null.TimeFrom(d.Created),
+	}
+}
+
+func (r *SqlBoilerUserRepository) Create(ctx context.Context, userDomain domain.User) (*domain.User, error) {
+	userModel := userDomainToModel(userDomain)
+	err := userModel.Insert(ctx, r.db, boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+	userDomain = userModelToDomain(userModel)
+	return &userDomain, nil
+}
+
+func (r *SqlBoilerUserRepository) GetAll(ctx context.Context) ([]domain.User, error) {
+	userModels, err := models.Users().All(ctx, r.db)
+	if err != nil {
+		return nil, err
+	}
+	return userModelsToDomains(userModels), nil
+}
+
+func (r *SqlBoilerUserRepository) GetById(ctx context.Context, id int) (*domain.User, error) {
+	userModel, err := models.FindUser(ctx, r.db, id)
+	if err != nil {
+		return nil, err
+	}
+	userDomain := userModelToDomain(*userModel)
+	return &userDomain, nil
+}
+
+func (r *SqlBoilerUserRepository) GetByUsername(ctx context.Context, username string) (*domain.User, error) {
+	userModel, err := models.Users(
+		qm.Where("username = ?", username),
+	).One(ctx, r.db)
+	if err != nil {
+		return nil, err
+	}
+	userDomain := userModelToDomain(*userModel)
+	return &userDomain, nil
+}
+
+func (r *SqlBoilerUserRepository) UpdateById(ctx context.Context, userDomain domain.User) (*domain.User, error) {
+	userModel := userDomainToModel(userDomain)
+	_, err := userModel.Update(ctx, r.db, boil.Infer())
+	if err != nil {
+		return nil, err
+	}
+	userDomain = userModelToDomain(userModel)
+	return &userDomain, nil
+}
+
+func (r *SqlBoilerUserRepository) DeleteById(ctx context.Context, userDomain domain.User) (*domain.User, error) {
+	userModel := userDomainToModel(userDomain)
+	_, err := userModel.Delete(ctx, r.db)
+	if err != nil {
+		return nil, err
+	}
+	userDomain = userModelToDomain(userModel)
+	return &userDomain, nil
+}
